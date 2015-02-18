258994eb5efc973b82f662d702c1d9814aba2a5a
Mikey <pmsorhaindo@gmail.com>
2015-02-18 22:27:21 +0000

    Initial Commit

---
+++ LoginTemplateApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginTemplateApp
+//
+//  Created by Paul-Michael Sorhaindo on 18/02/2015.
+//  Copyright (c) 2015 Paul-Michael Sorhaindo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginTemplateApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginTemplateApp
+//
+//  Created by Paul-Michael Sorhaindo on 18/02/2015.
+//  Copyright (c) 2015 Paul-Michael Sorhaindo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
